d02004aa555466f2812e03a4765f57d56c676e51
Marina Muñoz <marinacerejidoo@gmail.com>
2024-11-20 17:36:37 +0100

    Created the game frame

--- TypingGame.java
+++ TypingGame.java
@@ -1,5 +1,12 @@
+import javax.swing.*;
 public class TypingGame {
     public static void main(String[] args) {
         System.out.println("Welcome to the Typing Game!");
+
+        JFrame frame = new JFrame("Typing game");
+        frame.setSize(1000,400);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setVisible(true);
     }
 }
