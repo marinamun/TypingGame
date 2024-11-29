f2c282e0fa0d6f1c640c6657820d4a71aafa4633
Marina Muñoz <marinacerejidoo@gmail.com>
2024-11-29 09:58:06 +0100

    added a dummy http server for render to be happy

--- TypingGame.java
+++ TypingGame.java
@@ -1,6 +1,20 @@
+import java.net.InetSocketAddress;
+import com.sun.net.httpserver.HttpServer;
+
 import javax.swing.*;
 public class TypingGame {
-    public static void main(String[] args) {
+    public static void main(String[] args)throws Exception {
+        // Start a dummy HTTP server to satisfy Render's port requirement
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/", exchange -> {
+            String response = "Typing Game is running!";
+            exchange.sendResponseHeaders(200, response.getBytes().length);
+            exchange.getResponseBody().write(response.getBytes());
+            exchange.getResponseBody().close();
+        });
+        server.start();
+
+        System.out.println("Dummy HTTP server running on port 8080");
         System.out.println("Welcome to the Typing Game!");
 
         JFrame frame = new JFrame("Typing game");
